a043d731f1ac93d6e8a26f0c3c44ee51e75e4ad6
z <z@asl-dve-sil-MetaVR.asl-dve-sil>
2018-08-21 15:30:44 -0500

    Train Net2

--- models.py
+++ models.py
@@ -7,6 +7,10 @@ import torch.nn.functional as F
 # can use the below import should you choose to initialize the weights of your Net
 import torch.nn.init as I
 
+dtype = torch.cuda.FloatTensor
+device = torch.device("cuda:0")
+#nn = nn.cuda()
+
 class BasicNN(nn.Module):
     def __init__(self):
         super(BasicNN, self).__init__()
@@ -68,14 +72,13 @@ class Net2(nn.Module):
         super(Net2, self).__init__()
         
         ## Define all the layers of this CNN, the only requirements are:
-        ## 1. This network takes in a square (224 x 224), grayscale image as input
-        ## 2. It ends with a linear layer that represents the keypoints
-        ## last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        ## This network takes in a square (28 x 28), grayscale image as input
+        ## 10 output channels/feature maps
         
         # 1 - input image channel (grayscale), 32 output channels/feature maps, 4x4 square convolution kernel
         # 2x2 max pooling with 10% droupout
-        # ConvOut: (32, 221, 221) <-- (W-F+2p)/s+1 = (224 - 4)/1 + 1 
-        # PoolOut: (32, 110, 110) <-- W/s 
+        # ConvOut: (32, 25, 25) <-- (W-F+2p)/s+1 = (28 - 4)/1 + 1 
+        # PoolOut: (32, 12, 12) <-- W/s 
         self.conv1 = nn.Conv2d(1, 32, 4)
         self.bn1 = nn.BatchNorm2d(32)
         self.pool1 = nn.MaxPool2d(2, 2)
@@ -83,8 +86,8 @@ class Net2(nn.Module):
        
         # 2 - 64 output channels/feature maps, 3x3 square convolution kernel
         # 2x2 max pooling with 20% droupout
-        # ConvOut: (64, 108, 108) <-- (W-F+2p)/s+1 = (110 - 3)/1 + 1 
-        # PoolOut: (64, 54, 54) <-- W/s 
+        # ConvOut: (64, 10, 10) <-- (W-F+2p)/s+1 = (12 - 3)/1 + 1 
+        # PoolOut: (64, 5, 5) <-- W/s 
         self.conv2 = nn.Conv2d(32, 64, 3)
         self.bn2 = nn.BatchNorm2d(64)
         self.pool2 = nn.MaxPool2d(2, 2)
@@ -92,28 +95,20 @@ class Net2(nn.Module):
         
         # 3 - 128 output channels/feature maps, 2x2 square convolution kernel
         # 2x2 max pooling with 30% droupout
-        # ConvOut: (128, 53, 53) <-- (W-F+2p)/s+1 = (54 - 2)/1 + 1 
-        # PoolOut: (128, 26, 26) <-- W/s 
+        # ConvOut: (128, 4, 4) <-- (W-F+2p)/s+1 = (5 - 2)/1 + 1 
+        # PoolOut: (128, 2, 2) <-- W/s 
         self.conv3 = nn.Conv2d(64, 128, 2)
         self.bn3 = nn.BatchNorm2d(128)
         self.pool3 = nn.MaxPool2d(2, 2)
         self.dropout3 = nn.Dropout(p = 0.3)
         
-        # 4 - 64 output channels/feature maps, 3x3 square convolution kernel
-        # 2x2 max pooling with 10% droupout
-        # ConvOut: (256, 26, 26) <-- (W-F+2p)/s+1 = (26 - 1)/1 + 1 
-        # PoolOut: (256, 13, 13) <-- W/s 
-        self.conv4 = nn.Conv2d(128, 256, 1)
-        self.bn4 = nn.BatchNorm2d(256)
-        self.pool4 = nn.MaxPool2d(2, 2)
-        self.dropout4 = nn.Dropout(p = 0.4)
-        
         # Fully Connected Layers
-        self.fc1 = nn.Linear(256*13*13, 1000)
-        self.dropout5 = nn.Dropout(p = 0.5)
+        self.fc1 = nn.Linear(128*2*2, 1000)
+        self.dropout4 = nn.Dropout(p = 0.5)
         self.fc2 = nn.Linear(1000, 1000)
-        self.dropout6 = nn.Dropout(p = 0.6)
+        self.dropout5 = nn.Dropout(p = 0.6)
         self.fc3 = nn.Linear(1000, 136)
+        self.fc4 = nn.Linear(136, 10)
         
         
     def forward(self, x):
@@ -124,15 +119,16 @@ class Net2(nn.Module):
         x = self.dropout1(self.pool1(F.relu(self.bn1(self.conv1(x)))))
         x = self.dropout2(self.pool2(F.relu(self.bn2(self.conv2(x)))))
         x = self.dropout3(self.pool3(F.relu(self.bn3(self.conv3(x)))))
-        x = self.dropout4(self.pool4(F.relu(self.bn4(self.conv4(x)))))
 
         #flatten
         x = x.view(x.size(0), -1)
         
         #fully connected
-        x = self.dropout5(self.fc1(x))
-        x = self.dropout6(self.fc2(x))
+        x = self.dropout4(self.fc1(x))
+        x = self.dropout5(self.fc2(x))
         x = self.fc3(x)
+        # a softmax layer to convert the 10 outputs into a distribution of class scores
+        x = F.log_softmax(x, dim=1)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
